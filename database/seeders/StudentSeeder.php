9c0700bbbf6f7f70ca47465f63f472a850f81da4
GMP <GMP@Bryons-MacBook-Pro.local>
2023-07-13 00:32:37 +0300

    student model done migration seeder factory

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class StudentSeeder extends Seeder
+{
+    
+
+    public function run(): void
+    {
+        \App\Models\Student::factory()->count(10)->create();
+    }
+}
